3ab61b5d069695a1219199756fcb7c3e0be3528e
mjones2453 <mjones942676@gmail.com>
2025-03-12 20:54:39 -0400

    update to include dictionary before sort

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-    return file_contents
+    return file_contents.lower()
+
+# def sort_on(get_num_char):
+#     return letter_dict["key"]
+
+# def sort(sort_on):
+#     for key in letter_dict:
+#         letter_dict.sort(reverse=True, key=sort_on)
+#     print (letter_dict)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
+    letter_dict = get_num_char(book_text)
+    print("============ BOOKBOT ============")
+    print("Analying book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print (f"{num_words} words found in the document")
+    print("--------- Character Count ---------")
+    print(f"{letter_dict}")
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_num_words(file_contents):
     for i in file_contents.split():
         count += 1
     return count
+
+def get_num_char(file_contents):
+    count = 0
+    letter_dict = {}
+    for i in file_contents:
+        if i in letter_dict:
+            letter_dict[i] += 1
+        elif i not in letter_dict:
+            letter_dict[i] = 1
+    return letter_dict
+        
